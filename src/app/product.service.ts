3cbe90b6f9888de82e3b897c5637ddbab635880c
Verrya <63302524+Verrya@users.noreply.github.com>
2023-03-07 16:57:55 +0100

    Create product service

---
+++ src/app/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { environment } from 'src/environments/environment';
+import { SecondHandProduct } from './second-hand-product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  app = initializeApp(environment.firebase);
+  db = getFirestore(this.app);
+  constructor() {}
+
+  async getSecondHandProducts() {
+    const productsCol = collection(this.db, 'secondHandProducts');
+    const productSnapshot = await getDocs(productsCol);
+    const productList = productSnapshot.docs.map(doc => doc.data());
+    return productList;
+  }
+}

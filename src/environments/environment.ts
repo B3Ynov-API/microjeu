493cd4ae526da1aea3416c54453016a88313e14d
Verrya <63302524+Verrya@users.noreply.github.com>
2023-03-07 10:09:44 +0100

    install and config firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'microjeu-8bcd7',
+    appId: '1:119945304046:web:a17909be841d9974ce20ff',
+    databaseURL: 'https://microjeu-8bcd7-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'microjeu-8bcd7.appspot.com',
+    apiKey: 'AIzaSyAHrfTOdM-k23azIjmx1dzG1SFOI73do5A',
+    authDomain: 'microjeu-8bcd7.firebaseapp.com',
+    messagingSenderId: '119945304046',
+  },};
